4287e279ab57bbd3d482311d8b21f97d7c2ba1b3
Mario Obradovic <Mario.Obradovic@halcom.rs>
2021-09-25 15:06:06 +0200

    initial commit close#1

---
+++ src/main/java/com/firstcode/dispatchassist/DispatchAssistApplication.java
@@ -0,0 +1,13 @@
+package com.firstcode.dispatchassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchAssistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatchAssistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstcode/dispatchassist/DispatchAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstcode.dispatchassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchAssistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
